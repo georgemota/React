26754e77c7bfa64e526491312b2e155d83b4cf4d
George Mota <gamcustodio@outlook.com>
2023-10-06 11:45:52 -0400

    Revert "First steps in developer GifExpertApp"
    This reverts commit 517e1639934497402ca5cfaec103b53c758874d8.

--- 04-gif-expert-app/src/main.jsx
+++ 04-gif-expert-app/src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { GifExpertApp } from './GifExpertApp.jsx'
-import  './styles.css'
+import App from './App.jsx'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <GifExpertApp />
+    <App />
   </React.StrictMode>,
-);
+)
