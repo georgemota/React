166dd3036cb34fefd8aab5efb27197b08c8ff261
George Mota <gamcustodio@outlook.com>
2023-10-08 12:11:19 -0400

    Adding some finally changes of AddCategory comp

--- 04-gif-expert-app/src/components/AddCategory.jsx
+++ 04-gif-expert-app/src/components/AddCategory.jsx
@@ -1,6 +1,26 @@
+import { useState } from 'react';
 
 export const AddCategory = () => {
+
+  const [ inputValue, setInputValue ] = useState('Zoro')
+
+  const onInputChange = ({ target }) => {
+    setInputValue( target.value );
+  }
+
+  const onSubmit = ( event ) => {
+      event.preventDefault();
+  }
+
   return (
-    <div>AddCategory</div>
+    <form onSubmit={( event ) => onSubmit( event )}>
+      <input
+        type="text"
+        placeholder="Buscar gifs"
+        value={ inputValue }
+        onChange={ onInputChange }
+      />
+    </form>
+    
   )
 }
