45d2c58707c23c755a145c2587356b9b369d530d
George Mota <gamcustodio@outlook.com>
2023-10-31 13:13:58 -0400

    Adding peresonal-proyect-vite

---
+++ personal-proyect-vite/src/components/getImage.jsx
@@ -0,0 +1,24 @@
+
+// Tell webpack this JS file uses this imag
+export const getImage = () => {
+  return ( 
+    <>
+    <div>
+    <img src='..\images\logo.jpg'/>
+    <img src='..\images\logo2.jpg'/>
+    <img src='..\images\logo3.jpg'/>
+    <img src='..\images\logo4.jpg'/>
+    <img src='..\images\logo6.jpg'/>
+    <img src='..\images\logo7.jpg'/>
+    <img src='..\images\logo8.jpg'/>
+    <img src='..\images\logo9.jpeg'/>
+    <img src='..\images\logo10.jpeg'/>
+    <img src='..\images\logo11.jpeg'/>
+    
+    </div>
+    </>
+        
+  )
+}
+
+export default getImage;
